1e5e13ae0c0bf8148455cf49e510445ae454ef19
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-01-11 20:06:57 +0000

    Clearly not tree-shaking, and still meeting cryptic runtime error when ignoring node deps

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -3,6 +3,33 @@ const webpack = require("@nativescript/webpack");
 module.exports = (env) => {
 	webpack.init(env);
 
+	webpack.chainWebpack(config => {
+		config
+		.resolve.merge({
+			fallback: {
+				"util": false,
+				"bufferutil": false,
+				"utf-8-validate": false,
+				"stream": false,
+				"tty": false,
+				"path": false,
+				"os": false,
+				"zlib": false,
+				"net": false,
+				"crypto": false,
+				"http": false,
+				"https": false,
+				"url": false,
+				"assert": false,
+				"string_decoder": false,
+				"vm": false,
+				"child_process": false,
+				"canvas": false,
+				"buffer": false,
+			}
+		})
+	});
+
 	// Learn how to customize:
 	// https://docs.nativescript.org/webpack
 
