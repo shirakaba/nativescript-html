ba1cf020b72d32bac7c8ab33755d32772821f412
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-06-14 08:48:52 +0900

    Browserify Buffer, crypto, util, and stream

--- example/app/app.ts
+++ example/app/app.ts
@@ -1,4 +1,11 @@
 import { Application } from '@nativescript/core'
+import HTMLDocument from '../../node_modules/happy-dom/lib/nodes/html-document/HTMLDocument';
+
+console.log('creating document 2...');
+const document = new HTMLDocument();
+console.log('creating div...');
+const div = document.createElement('div');
+console.log(div);
 
 Application.run({ moduleName: 'app-root' })
 
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,10 +1,26 @@
 const webpack = require("@nativescript/webpack");
+const ProvidePlugin = require.resolve('webpack/lib/ProvidePlugin');
 
 module.exports = (env) => {
 	webpack.init(env);
 
 	// Learn how to customize:
 	// https://docs.nativescript.org/webpack
+
+	webpack.chainWebpack(config => {
+		config.resolve.set('fallback', {
+			Buffer: require.resolve('buffer'),
+			crypto: require.resolve('crypto-browserify'),
+			util: require.resolve('util'),
+			stream: require.resolve('stream-browserify'),
+		});
+	
+		config
+		.plugin('BufferPlugin') // arbitrary name
+		.use(ProvidePlugin, [
+			{ Buffer: ['buffer', 'Buffer'] },
+		]);
+	});
 
 	return webpack.resolveConfig();
 };
