d0f4a342cdd8429294398123c568b291bdfede5c
shirakaba <14055146+shirakaba@users.noreply.github.com>
2022-06-14 22:05:11 +0900

    Example of implementing fallbacks for Node APIs

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -8,6 +8,42 @@ module.exports = (env) => {
 	// https://docs.nativescript.org/webpack
 
 	webpack.chainWebpack(config => {
+		// config.resolve.set('fallback', {
+		// 	Buffer: require.resolve('buffer'),
+		// 	crypto: require.resolve('crypto-browserify'),
+		// 	util: require.resolve('util'),
+		// 	stream: require.resolve('stream-browserify'),
+		// 	'zlib': false,
+		// 	'url': false,
+		// 	'path': false,
+		// 	'net': false,
+		// 	'child_process': false,
+		// 	'domain': false,
+		// 	'fs': false,
+		// 	'http': false,
+		// 	'https': false,
+		// });
+	
+		// config
+		// .plugin('BufferPlugin') // arbitrary name
+		// .use(require.resolve('webpack/lib/ProvidePlugin'), [
+		// 	{ Buffer: ['buffer', 'Buffer'] },
+		// ]);
+
+		// config.plugin('NormalModuleReplacementPlugin')
+		// .use(
+		// 	require.resolve('webpack/lib/NormalModuleReplacementPlugin');,
+		// 	[
+		// 		/.*\/happy-dom\/lib\/fetch\/ResourceFetchHandler(\.js)?$/,
+		// 		function(resource){
+		// 			console.log(`!! resource`, resource);
+		// 			console.log(`!! will replace resource.request with:`, path.resolve(__dirname, "lib", "ResourceFetchHandler.js"));
+
+		// 			resource.request = path.resolve(__dirname, "lib", "ResourceFetchHandler.js");
+		// 		}
+		// 	]
+		// );
+
 		// Swap out their ResourceFetchHandler for ours (which uses
 		// NativeScript's global fetch() rather than their Node-based one).
 		// Responsible for this error (that seems to be inconsequential..?):
